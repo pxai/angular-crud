4e40756594365d8a24cda93b8c7c4d741111ad0a
pxai <pello_altadill@cuatrovientos.org>
2016-04-20 18:05:12 +0200

    some minor fixes

--- public/js/services.js
+++ public/js/services.js
@@ -3,9 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-
 var module = angular.module('classAppServices', ['ngResource']);
 
 module.factory('Friends', ['$http', function ($http) {
