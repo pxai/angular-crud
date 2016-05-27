cea4723c674646ea0e67cce638292bb3b06af817
Your Name <you@example.com>
2016-05-27 11:47:50 +0200

    project working

---
+++ conf.js
@@ -0,0 +1,13 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec/*spec.js']
+};
+
+/*
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec/*spec.js'],
+  capabilities: {
+   browserName: 'firefox',
+ }
+*/
