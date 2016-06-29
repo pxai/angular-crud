5f3a9ae085afa111cf6142ebc078b5f6008ca8bf
pxai <pello_altadill@cuatrovientos.org>
2016-06-29 23:53:36 +0200

    changed karma conf file

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,7 @@
 module.exports = function(config) {
   config.set({
-    browsers : ['Chrome', 'Firefox']
+    browsers : ['Chrome', 'Firefox'],
+    basePath: '../..',
+    frameworks: ['jasmine']
   });
 };
