2232fcab1e4a7fc237b2d9570c3e609bb55669da
pxai <pello_altadill@cuatrovientos.org>
2016-06-29 23:59:46 +0200

    changed karma conf file, now complete

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,28 @@
 module.exports = function(config) {
   config.set({
-    browsers : ['Chrome', 'Firefox'],
-    basePath: '../..',
-    frameworks: ['jasmine']
+ basePath: './app',
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      '**/*.module.js',
+      '*!(.module|.spec).js',
+      '!(bower_components)/**/*!(.module|.spec).js',
+      '**/*.spec.js'
+],
+   autoWatch: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['Chrome', 'Firefox'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine'
+]
   });
 };
